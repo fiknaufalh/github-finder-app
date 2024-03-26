8e8f175b5fdc5358f7142775b91bd87cce1b2970
fiknaufalh <18221096@mahasiswa.itb.ac.id>
2024-03-26 08:19:06 +0700

    refactor: update handler arg and refactor delay

--- app/src/main/java/com/fiknaufalh/githubfinder/ui/activities/SplashActivity.kt
+++ app/src/main/java/com/fiknaufalh/githubfinder/ui/activities/SplashActivity.kt
@@ -4,17 +4,23 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 import com.fiknaufalh.githubfinder.R
 
 class SplashActivity : AppCompatActivity() {
+
+    companion object {
+        const val delayTime: Long = 2000
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        Handler().postDelayed({
+        Handler(Looper.getMainLooper()).postDelayed({
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
-        }, 2000)
+        }, delayTime)
     }
 }
