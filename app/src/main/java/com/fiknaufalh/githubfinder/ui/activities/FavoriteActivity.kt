ee4aeea11fec8b80f95a0408f42b24c9543758e0
fiknaufalh <18221096@mahasiswa.itb.ac.id>
2024-03-28 15:57:47 +0700

    feat: add favorite activity plain

---
+++ app/src/main/java/com/fiknaufalh/githubfinder/ui/activities/FavoriteActivity.kt
@@ -0,0 +1,12 @@
+package com.fiknaufalh.githubfinder.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.fiknaufalh.githubfinder.R
+
+class FavoriteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_favorite)
+    }
+}
